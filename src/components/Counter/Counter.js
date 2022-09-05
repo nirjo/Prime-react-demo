6e98feaab357ad572e42014a3ce072d6c0ac638e
nirjo <32131587+nirjo@users.noreply.github.com>
2022-09-05 11:37:32 +0530

    1 binaries uploaded

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,37 @@
 import React, { useState, useRef } from 'react';
+import { Button } from 'primereact/button';
 
 import { Card } from 'primereact/card';
 
 const Counter = () => {
+  const header = <img alt="Card" src="images/usercard.png" />;
+  const footer = (
+    <span>
+      <Button
+        label="Save"
+        icon="pi pi-check"
+        style={{ marginRight: '.25em' }}
+      />
+      <Button
+        label="Cancel"
+        icon="pi pi-times"
+        className="p-button-secondary"
+      />
+    </span>
+  );
   return (
     <div className="Grid">
       <div className="col-4"></div>
       <React.Fragment>
-        <Card className="bg-black-alpha-20 m-4"> Cards</Card>
+        <Card>
+          <Card
+            className="bg-black-alpha-20 m-4"
+            footer={footer}
+            header={header}
+          >
+            CARDS
+          </Card>
+        </Card>
       </React.Fragment>
     </div>
   );
